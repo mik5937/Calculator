cfbbab5837568f7712222ca4423853b2dbd70fe2
mik5937 <54955142+mik5937@users.noreply.github.com>
2020-06-08 12:21:39 +0700

    Add files via upload

---
+++ Calculator/src/SecondNumPack/Main.java
@@ -0,0 +1,12 @@
+package SecondNumPack;
+//package Calculator;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        calc.calculations();
+    }
+}
